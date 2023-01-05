ba0d07c56df48a16e0908c59d57a0e90772a1b14
chebishev <atanas.chebishev@gmail.com>
2023-01-05 21:19:21 +0200

    Lists as Stacks and Queues - Exercise -> 8/10

--- 20220913_Exercise_Lists_As_Stacks_And_Queues/crossroads.py
+++ 20220913_Exercise_Lists_As_Stacks_And_Queues/crossroads.py
@@ -2,37 +2,45 @@ from collections import deque
 
 green_light_duration = int(input())
 free_window_duration = int(input())
+current_car = ""
 cars_queue = deque()
 cars_passed = []
+crash = False
 while True:
+    if crash:
+        break
     command = input()
     if command == "END":
         print("Everyone is safe.")
         print(f"{len(cars_passed)} total cars passed the crossroads.")
         break
+
     elif command == "green":
-        if cars_queue:
-            current_car = cars_queue.popleft()
-            cars_passed.append(current_car)
-            for second in range(green_light_duration):
-                current_car = current_car[1:]
-                if not current_car:
-                    if cars_queue:
-                        current_car = cars_queue.popleft()
-                        cars_passed.append(current_car)
+        time = green_light_duration
+        while time:
+            if cars_queue:
+                current_car = cars_queue.popleft()
+                cars_passed.append(current_car)
+                if len(current_car) > time:
+                    current_car = current_car[time:]
+                    time = 0
+                else:
+                    time -= len(current_car)
+                    if time > 0:
+                        continue
                     else:
                         break
             else:
-                if current_car:
-                    for second in range(free_window_duration):
-                        current_car = current_car[1:]
-                        if not current_car:
-                            break
-                    else:
-                        if current_car:
-                            print("A crash happened!")
-                            print(f"{cars_passed.pop()} was hit at {current_car[0]}.")
-                            break
+                break
+
+            if current_car:
+                if len(current_car) > free_window_duration:
+                    current_car = current_car[free_window_duration:]
+                    print("A crash happened!")
+                    print(f"{cars_passed.pop()} was hit at {current_car[0]}.")
+                    crash = True
+                    break
+
     else:
         cars_queue.append(command)
 

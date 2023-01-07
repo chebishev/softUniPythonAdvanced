500e43b91ad0ce4100dafa8def7374a8d8d25910
chebishev <atanas.chebishev@gmail.com>
2023-01-07 17:05:04 +0200

    Lists as Stacks and Queues - Exercise -> Complete

--- 20220913_Exercise_Lists_As_Stacks_And_Queues/cups_and_bottles.py
+++ 20220913_Exercise_Lists_As_Stacks_And_Queues/cups_and_bottles.py
@@ -9,12 +9,26 @@ while cups and bottles_stack:
         current_bottle = bottles_stack.pop()
         if current_bottle - current_cup < 0:
             current_cup -= current_bottle
-            wasted_watter += current_bottle
             continue
         elif current_bottle - current_cup > 0:
             current_bottle -= current_cup
-            wasted_watter += current_cup
-            if cups:
-                current_cup = cups.popleft()
-print(cups)
-print(bottles_stack)
+            wasted_watter += current_bottle
+            break
+        else:
+            break
+if cups:
+    print(f"Cups:", *cups, sep=' ')
+elif bottles_stack:
+    print(f"Bottles:", *bottles_stack, sep=" ")
+print(f"Wasted litters of water: {wasted_watter}")
+
+# test inputs:
+
+# 4 2 10 5
+# 3 15 15 11 6
+
+# 1 5 28 1 4
+# 3 18 1 9 30 4 5
+
+# 10 20 30 40 50
+# 20 11

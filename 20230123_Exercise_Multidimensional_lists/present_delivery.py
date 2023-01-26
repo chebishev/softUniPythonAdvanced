1a36fdda68fc526189f47068dc74ef4d2766b4fc
chebishev <atanas.chebishev@gmail.com>
2023-01-26 12:53:35 +0200

    Multidimensional Lists - Exercise 2 7/7

--- 20230123_Exercise_Multidimensional_lists/present_delivery.py
+++ 20230123_Exercise_Multidimensional_lists/present_delivery.py
@@ -3,35 +3,6 @@ def position_is_valid(row_index, col_index):
         return True
 
 
-def cookie_found(row_index, col_index, presents, kids):
-    cookie_radius = (
-        (0, 0),  # current_position
-        (-1, -1),  # up_left_diagonal
-        (-1, 0),  # up
-        (-1, 1),  # up_right_diagonal
-        (0, -1),  # left
-        (0, 1),  # right
-        (1, -1),  # down_left_diagonal
-        (1, 0),  # down
-        (1, 1)  # down_right_diagonal
-    )
-    for position in cookie_radius:
-        new_row = row_index + position[0]
-        new_col = col_index + position[1]
-        new_spot = neighbourhood[new_row][new_col]
-        if position_is_valid(new_row, new_col):
-            if presents:
-                if new_spot == "V":
-                    presents -= 1
-                    kids += 1
-                elif new_spot == "X":
-                    presents -= 1
-                neighbourhood[new_row][new_col] = "-"
-        if not presents:
-            return presents, kids
-        return presents, kids
-
-
 number_of_presents = int(input())
 neighbourhood_size = int(input())
 neighbourhood = []
@@ -63,10 +34,23 @@ while True:
     if position_is_valid(next_row, next_col):
         current_position = neighbourhood[next_row][next_col]
         if current_position == "C":
-            number_of_presents, nice_kids_visited = cookie_found(next_row, next_col, number_of_presents, nice_kids_visited)
+            for position in directions:
+                new_row = next_row + directions[position][0]
+                new_col = next_col + directions[position][1]
+                new_spot = neighbourhood[new_row][new_col]
+                if number_of_presents:
+                    if new_spot == "V":
+                        nice_kids_visited += 1
+                        number_of_presents -= 1
+                    elif new_spot == "X":
+                        number_of_presents -= 1
+                    neighbourhood[new_row][new_col] = "-"
+                else:
+                    break
 
         elif current_position == "V":
             nice_kids_visited += 1
+            number_of_presents -= 1
         neighbourhood[next_row][next_col] = "-"
         santa_position = [next_row, next_col]
 
@@ -78,3 +62,8 @@ while True:
 neighbourhood[santa_position[0]][santa_position[1]] = "S"
 for row in neighbourhood:
     print(' '.join(x for x in row))
+
+if all_nice_kids == nice_kids_visited:
+    print(f"Good job, Santa! {all_nice_kids} happy nice kid/s.")
+else:
+    print(f"No presents for {all_nice_kids - nice_kids_visited} nice kid/s.")

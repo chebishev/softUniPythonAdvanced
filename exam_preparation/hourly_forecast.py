ecaa6ed658025e675896e76fd550966a1f8a610f
chebishev <atanas.chebishev@gmail.com>
2023-01-30 12:23:13 +0200

    Python Advanced Exam - 22 October 2022  completed

--- exam_preparation/hourly_forecast.py
+++ exam_preparation/hourly_forecast.py
@@ -1,17 +1,18 @@
 def forecast(*weather):
+    # for easier sorting of strings in descending order with lambda and other value for sorting
     weather_translate = {
         "Sunny": 1,
         "Rainy": 3,
         "Cloudy": 2
     }
-    weather_cities = {}
-    weather_dictionary = {}
+    weather_cities = {}    # corresponding weather for the current city
+    weather_dictionary = {}  # number instead of weather for easier sorting
     for item in weather:
-        weather_dictionary[item[0]] = weather_translate[item[1]]
-        weather_cities[item[0]] = item[1]
+        weather_dictionary[item[0]] = weather_translate[item[1]]  # filling numbers as values
+        weather_cities[item[0]] = item[1]   # filling weather as value
     sorted_weather = dict(sorted(weather_dictionary.items(), key=lambda x: (x[1], x[0])))
     output_list = []
-    for k in sorted_weather:
+    for k in sorted_weather:        # getting the weather condition by current city
         output_list.append(f"{k} - {weather_cities[k]}")
 
     return "\n".join(output_list)

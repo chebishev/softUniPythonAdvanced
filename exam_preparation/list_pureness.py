9787cc0137bcf2685aa2e60ac3f082dea906ed2c
chebishev <atanas.chebishev@gmail.com>
2023-02-13 16:17:02 +0200

    Python Advanced Exam - 24 October 2020 - Completed

--- exam_preparation/list_pureness.py
+++ exam_preparation/list_pureness.py
@@ -4,23 +4,21 @@ from collections import deque
 
 def best_list_pureness(*args):
     lst = deque(args[0])
-    if lst:
-        rotations = 0
-        pure_rotation = 0
-        max_pureness = -sys.maxsize
-        if args[1]:
-            for _ in range(args[1]):
-                current_pureness = sum(index * value for index, value in enumerate(lst))
-                if current_pureness > max_pureness:
-                    max_pureness = current_pureness
-                    pure_rotation = rotations
-                lst.rotate()
-                rotations += 1
-            return f"Best pureness {max_pureness} after {pure_rotation} rotations"
-        else:
-            return f"Best pureness {sum(index * value for index, value in enumerate(lst))} after {0} rotations"
-    else:
-        return f"Best pureness {0} after {0} rotations"
+    rotations = 0
+    pure_rotation = 0
+    max_pureness = -sys.maxsize
+
+    for _ in range(len(lst)):
+        current_pureness = sum(index * value for index, value in enumerate(lst))
+
+        if current_pureness > max_pureness:
+            max_pureness = current_pureness
+            pure_rotation = rotations
+
+        lst.rotate()
+        rotations += 1
+
+    return f"Best pureness {max_pureness} after {pure_rotation} rotations"
 
 
 test = ([4, 3, 2, 6], 4)

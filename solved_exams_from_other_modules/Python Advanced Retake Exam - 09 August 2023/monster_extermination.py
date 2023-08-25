e2760c67c5468d617df4741fcc637ea8a9a0a47a
chebishev <atanas.chebishev@gmail.com>
2023-08-25 11:05:35 +0300

    exams folder refactored

---
+++ solved_exams_from_other_modules/Python Advanced Retake Exam - 09 August 2023/monster_extermination.py
@@ -0,0 +1,7 @@
+# The first sequence represents the armor of the monsters.
+# Each integer value represents the armor of a different monster.
+monsters_armor = deque([])
+
+# The second sequence represents the soldier's striking impact.
+# Each integer value represents the strength of a strike performed by the soldier.
+striking_impact = []
